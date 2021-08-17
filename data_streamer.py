41148b4dee2edc85acc649aacc52c9e85315c8a5
elifehacker <steven_wang_pei@hotmail.com>
2021-08-17 14:41:28 +1000

    too much indent

--- data_streamer.py
+++ data_streamer.py
@@ -5,29 +5,29 @@ import requests
 import time
 
 with open(MESSAGE_BUS_PATH, "r") as f:
-        # simulate message coming in
-        line_count = 0
-        while True:
-            line = f.readline() 
-            line_count += 1
-            if not line:
-                break
-            # keep sending if a message is bogus
-            try:
-                msg = json.loads(line)
-                msg['timestamp'] = datetime.now()
-                url = 'http://127.0.0.1:5000/api/message'
-                print('sending data',msg)
-                resp = requests.post(
-                    url=url,
-                    json=json.dumps(msg, indent=4, sort_keys=True, default=str)
-                )
-                print(resp.status_code)
-                print(resp.text)
-            except Exception as e:
-                write_to_error_log(line, e, SENDER_ERROR_LOG_PATH)
+    # simulate message coming in
+    line_count = 0
+    while True:
+        line = f.readline() 
+        line_count += 1
+        if not line:
+            break
+        # keep sending if a message is bogus
+        try:
+            msg = json.loads(line)
+            msg['timestamp'] = datetime.now()
+            url = 'http://127.0.0.1:5000/api/message'
+            print('sending data',msg)
+            resp = requests.post(
+                url=url,
+                json=json.dumps(msg, indent=4, sort_keys=True, default=str)
+            )
+            print(resp.status_code)
+            print(resp.text)
+        except Exception as e:
+            write_to_error_log(line, e, SENDER_ERROR_LOG_PATH)
 
-            time.sleep(5)
+        time.sleep(5)
 
 '''
 docker build --tag python-docker .
